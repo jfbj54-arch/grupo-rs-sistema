739a568167ce2bd12ee9d3106d5e697fd4a9d456
jfbj54-arch <jfbj54@gmail.com>
2026-04-09 17:15:14 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,21 +1,31 @@
-// Firebase v8 (compat) - funciona com firebase.auth() e firebase.firestore()
+// Carregar Firebase v8 corretamente, sem document.write
 
-document.write(`
-<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js"></script>
-`);
+// Carrega os scripts do Firebase
+const scriptApp = document.createElement("script");
+scriptApp.src = "https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js";
+document.head.appendChild(scriptApp);
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBFaCj_q5I6r1Hd6iOSoGmx8tbYnVaCAcE",
-  authDomain: "grupo-rs-prestadora.firebaseapp.com",
-  projectId: "grupo-rs-prestadora",
-  storageBucket: "grupo-rs-prestadora.appspot.com",
-  messagingSenderId: "1090467227115",
-  appId: "1:1090467227115:web:29e591100fbe0c87b165ae"
+const scriptAuth = document.createElement("script");
+scriptAuth.src = "https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js";
+document.head.appendChild(scriptAuth);
+
+const scriptFirestore = document.createElement("script");
+scriptFirestore.src = "https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js";
+document.head.appendChild(scriptFirestore);
+
+// Inicializa o Firebase quando todos os scripts carregarem
+scriptFirestore.onload = () => {
+    const firebaseConfig = {
+        apiKey: "AIzaSyBFaCj_qS16rIHd6iOsoGmx8tbYnVcAcAE",
+        authDomain: "grupo-rs-prestadora.firebaseapp.com",
+        projectId: "grupo-rs-prestadora",
+        storageBucket: "grupo-rs-prestadora.appspot.com",
+        messagingSenderId: "1090462727115",
+        appId: "1:1090462727115:web:29e59110e0bc687b165ae"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+
+    window.auth = firebase.auth();
+    window.db = firebase.firestore();
 };
-
-firebase.initializeApp(firebaseConfig);
-
-const auth = firebase.auth();
-const db = firebase.firestore();
