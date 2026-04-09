a77c1f492c8149f7b2a92cea6b446e9d18af3e01
jfbj54-arch <jfbj54@gmail.com>
2026-04-09 13:52:47 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,19 +1,21 @@
-<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js"></script>
+// Firebase v8 (compat) - funciona com firebase.auth() e firebase.firestore()
 
-<script>
-  const firebaseConfig = {
-    apiKey: "AIzaSyBFaCj_q5I6r1Hd6iOSoGmx8tbYnVaCAcE",
-    authDomain: "grupo-rs-prestadora.firebaseapp.com",
-    projectId: "grupo-rs-prestadora",
-    storageBucket: "grupo-rs-prestadora.firebasestorage.app",
-    messagingSenderId: "1090467227115",
-    appId: "1:1090467227115:web:29e591100fbe0c87b165ae"
-  };
+document.write(`
+<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js"></script>
+`);
 
-  firebase.initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: "AIzaSyBFaCj_q5I6r1Hd6iOSoGmx8tbYnVaCAcE",
+  authDomain: "grupo-rs-prestadora.firebaseapp.com",
+  projectId: "grupo-rs-prestadora",
+  storageBucket: "grupo-rs-prestadora.appspot.com",
+  messagingSenderId: "1090467227115",
+  appId: "1:1090467227115:web:29e591100fbe0c87b165ae"
+};
 
-  const auth = firebase.auth();
-  const db = firebase.firestore();
-</script>
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
