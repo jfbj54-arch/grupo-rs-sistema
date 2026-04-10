7743db3742d061c3daae8d36203596851c116ef6
jfbj54-arch <jfbj54@gmail.com>
2026-04-10 12:03:42 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,4 +1,4 @@
-// Carregar Firebase v8 corretamente, sem document.write
+/// Carregar Firebase v8 corretamente, sem document.write
 
 // Carrega os scripts do Firebase
 const scriptApp = document.createElement("script");
@@ -16,13 +16,12 @@ document.head.appendChild(scriptFirestore);
 // Inicializa o Firebase quando todos os scripts carregarem
 scriptFirestore.onload = () => {
     const firebaseConfig = {
-    apiKey: "AIzaSyBFaCj_qS16rIHd6iOsoGmx8tbYnVcAcAE",
-    authDomain: "grupo-rs-prestadora.firebaseapp.com",
-    projectId: "grupo-rs-prestadora",
-    storageBucket: "grupo-rs-prestadora.appspot.com",
-    messagingSenderId: "1090462727115",
-    appId: "1:1090462727115:web:29e59110e0bc687b165ae"
-};
+        apiKey: "AIzaSyBFaCj_qS16rIHd6iOsoGmx8tbYnVcAcAE",
+        authDomain: "grupo-rs-prestadora.firebaseapp.com",
+        projectId: "grupo-rs-prestadora",
+        storageBucket: "grupo-rs-prestadora.appspot.com",
+        messagingSenderId: "1090462727115",
+        appId: "1:1090462727115:web:29e59110e0bc687b165ae"
     };
 
     firebase.initializeApp(firebaseConfig);
